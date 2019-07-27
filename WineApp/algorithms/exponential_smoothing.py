7a025af1d1790c05894bdc655b9c9373104a39f0
AndreaPuccia <andreapuccia97@gmail.com>
2019-07-27 15:36:25 +0200

    Error evaluation

--- WineApp/algorithms/exponential_smoothing.py
+++ WineApp/algorithms/exponential_smoothing.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import math
+import numpy as np
 from statsmodels.tsa.holtwinters import ExponentialSmoothing
 
 from WineApp.models import SensorHistory, DailySensorData
@@ -10,19 +12,26 @@ def exponential_smoothing(field):
     plt.show()
     ss = 0.45
     sl = 0.6
-    if field == 'airTemperatureAvg' or field == 'dewPointAvg':
+    if field == 'airTemperatureAvg' or field == 'dewPointAvg' \
+            or field == 'airTemperatureMax' or field == 'airTemperatureMin':
         model = ExponentialSmoothing(train, trend='add', seasonal='add', seasonal_periods=365)
     else:
         model = ExponentialSmoothing(train, trend='add', seasonal=None)
     fit_model = model.fit(smoothing_seasonal=ss, smoothing_level=sl)
 
-    title = ""+str(ss)+" "+str(sl)+" "+field
+    title = "" + str(ss) + " " + str(sl) + " " + field
 
     pred = fit_model.forecast(10)
+
+    anomaly1, anomaly2, anomaly3 = detect_anomalies(test, pred)
+
     plt.title(title)
     plt.plot(pred, '-r', label='Predictions')
-    plt.plot(test, 'b', label='Actual')
-
+    plt.plot(test, '-b', label='Actual')
+    # Plot anomalies
+    plt.plot(anomaly1, 'og', label='std', markersize=17)
+    plt.plot(anomaly2, 'vy', label='stdev_corr', markersize=12)
+    plt.plot(anomaly3, 'sm', label='stdev welford', markersize=5)
     plt.legend()
     plt.show()
     actual = test
@@ -37,14 +46,63 @@ def _get_series(field):
     dates = data.values_list('date', flat=True)
     test_values = data.values_list(field, flat=True)
 
-    train_values = _convert_to_vector(train_values)
-    test_values = _convert_to_vector(test_values)
+    train_values = list(train_values)
+    test_values = list(test_values)
 
     return train_values, test_values, dates
 
 
-def _convert_to_vector(set):
-    result = []
-    for i in set:
-        result.append(i)
-    return result
+def detect_anomalies(actual, prediction):
+    error = actual - prediction
+    anomaly1 = []
+    anomaly2 = []
+    anomaly3 = []
+
+    # stddev calculate at the beginning
+    std = error.std()
+    print(std)
+    for i in range(0, len(error)):
+        if abs(error[i]) > 3 * std:
+            anomaly1.append(actual[i])
+        else:
+            anomaly1.append(np.NaN)
+    print(std)
+
+    # stdev_corr
+    mean = 0
+    var = 0
+    count = 0
+    for i in range(0, len(error)):
+        new_mean, new_var = update(mean, var, error[i], count + 1)
+        std = math.sqrt(new_var / i)
+        if abs(error[i]) > 3 * std:
+            anomaly2.append(actual[i])
+        else:
+            anomaly2.append(np.NaN)
+            mean = new_mean
+            var = new_var
+            count = count + 1
+    print(std)
+
+    # stdev
+    mean = 0
+    var = 0
+    for i in range(0, len(error)):
+        mean, var = update(mean, var, error[i], i + 1)
+        std = math.sqrt(var / i)
+        if abs(error[i]) > 3 * std:
+            anomaly3.append(actual[i])
+        else:
+            anomaly3.append(np.NaN)
+    print(std)
+
+    plt.plot(error, '-g')
+    plt.title('Error')
+    plt.show()
+    return anomaly1, anomaly2, anomaly3
+
+
+def update(m, v, val, n):
+    mean = m + (1 / n) * (val - m)
+    var = v + (val - m) * (val - mean)
+    return mean, var
