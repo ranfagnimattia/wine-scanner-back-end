207a0e8c3141db38fbe5341a9f0a73697036d04d
AndreaPuccia <andreapuccia97@gmail.com>
2019-08-07 16:27:42 +0200

    Add last month and trend graphics

--- WineApp/static/WineApp/js/daily_data.js
+++ WineApp/static/WineApp/js/daily_data.js
@@ -1,6 +1,16 @@
 $('document').ready(function () {
+    console.log('all data');
     console.log(data_py);
-    updateChart(data_py.sensor, data_py.data);
+
+    let sensor = data_py.sensor;
+    let monthData = lastMonthData(data_py.data);//test
+
+    updateChart(sensor, data_py.data);
+    setUpButton(sensor, 'month');
+    setUpButton(sensor, 'trend');
+    updateOtherChart(sensor, monthData, 'month-chart');
+    // Passare i dati dell'andamento generale al posto di month chart
+    updateOtherChart(sensor, monthData, 'trend-chart');
 
     $('.sensor').on('click', function () {
         console.log("click");
@@ -13,11 +23,34 @@ $('document').ready(function () {
             success: function (response) {
                 if (response) {
                     console.log(response);
-                    updateChart(response.sensor, response.data);
+
+                    sensor = response.sensor;
+                    monthData = lastMonthData(response.data);
+
+                    updateChart(sensor, response.data);
+                    setUpButton(sensor, 'month');
+                    setUpButton(sensor, 'trend');
+                    updateOtherChart(sensor, monthData, 'month-chart');
+                    // Passare i dati dell'andamento generale al posto di month chart
+                    updateOtherChart(sensor, monthData, 'trend-chart')
                 }
             }
         });
     });
+    /*Gestore dell'evento click sui bottoni del grafico dell'ultimo mese*/
+    $('.month').on('click', function () {
+        console.log("month click");
+        const measure = $(this).attr('data-val');
+        if (!$(this).hasClass("active"))
+            updateOtherChart(sensor, monthData, 'month-chart', measure)
+    });
+    /*Gestore dell'evento click sui bottoni del grafico dell'andamento*/
+    $('.trend').on('click', function () {
+        console.log("trend click");
+        const measure = $(this).attr('data-val');
+        if (!$(this).hasClass("active"))
+            updateOtherChart(sensor, monthData, 'trend-chart', measure)
+    })
 });
 
 
@@ -89,4 +122,114 @@ function updateChart(sensor, data) {
             }]
         }
     });
+}
+/*Aggiorno i frafici secondari*/
+function updateOtherChart(sensor, data, id, measure = "Avg") {
+    let plot;
+    let subcaption;
+    let schema;
+    let caption;
+    let chartData;
+    const chartElem = $('#' + id);
+    if (sensor.tot) {
+        schema = [
+            {
+                name: "Time",
+                type: "date",
+                format: "%Y-%m-%d"
+            },
+            {
+                name: "Tot",
+                type: "number"
+            }];
+        subcaption = sensor.name + ' Tot ';
+        plot = [{"value": "Tot"}];
+        chartData = getMeasure('Tot',data)
+    } else {
+        schema = [
+            {
+                name: "Time",
+                type: "date",
+                format: "%Y-%m-%d"
+            },
+            {
+                name: measure,
+                type: "number"
+            }];
+        subcaption = sensor.name + ' ' + measure;
+        plot = [{"value": measure}];
+        chartData = getMeasure(measure,data)
+    }
+    let format = {"suffix": sensor.unit};
+    if (id === 'month-chart') {
+        caption = 'Ultimo Mese';
+    } else {
+        caption = 'Andamento';
+    }
+
+    const fusionDataStore = new FusionCharts.DataStore();
+    const fusionTable = fusionDataStore.createDataTable(chartData, schema);
+
+    chartElem.find('.card-title').text(caption);
+    chartElem.find('.card-category').text(subcaption);
+    chartElem.find('.chart').insertFusionCharts({
+        type: 'timeseries',
+        width: '100%',
+        height: '100%',
+        dataFormat: 'json',
+        dataSource: {
+            chart: {
+                theme: 'candy',
+            },
+            data: fusionTable,
+            yAxis: [{
+                "plot": plot,
+                "format": format,
+                title: ''
+            }]
+        }
+    });
+}
+/* Iniziallizzo i bottoni dei grafici*/
+function setUpButton(sensor, id) {
+    if (sensor.tot) {
+        let tot = $('#' + id + '-tot');
+        tot.removeAttr("disabled");
+        tot.addClass("active");
+        $('#' + id + '-avg').attr("disabled", "disabled");
+        $('#' + id + '-min').attr("disabled", "disabled");
+        $('#' + id + '-max').attr("disabled", "disabled");
+    } else {
+        let avg = $('#' + id + '-avg');
+        avg.removeAttr("disabled");
+        $('#' + id + '-min').removeAttr("disabled");
+        $('#' + id + '-max').removeAttr("disabled");
+        avg.addClass("active");
+        $('#' + id + '-tot').attr("disabled", "disabled")
+    }
+}
+/*Recupero i dati relativi all'ultimo mese*/
+function lastMonthData(allData) {
+    let len = allData.length;
+    let monthData = [];
+    for (let i = 30; i > 0; i--) {
+        if (i <= len) {
+            monthData.push(allData[len - i])
+        }
+    }
+    console.log('lastMonthData');
+    console.log(monthData);
+    return monthData
+}
+/*Recupero i dati relativi ad una singola misura max,min,tot o avg*/
+function getMeasure(measure,data) {
+    let dict = {'Avg':1,'Max':3,'Min':2,'Tot':1};
+    let j = dict[measure];
+    let measureData = [];
+    for(let i=0;i<data.length;i++){
+        measureData.push([data[i][0],data[i][j]])
+    }
+    console.log('measure data '+measure);
+    console.log(measureData);
+    return measureData
 }
