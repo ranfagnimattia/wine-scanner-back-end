db5f645841a0bf13c6d41e90db175cbe78d39494
AndreaPuccia <andreapuccia97@gmail.com>
2019-09-08 18:51:07 +0200

    Clean up project

--- WineApp/models.py
+++ WineApp/models.py
@@ -231,63 +231,6 @@ class Prediction(models.Model):
                str(self.measure) + '-' + str(self.method)
 
 
-class VintageRating(models.Model):
-    year = models.IntegerField(primary_key=True)
-    rating = models.FloatField(
-        validators=[
-            MaxValueValidator(5.0),
-            MinValueValidator(1.0)
-        ]
-    )
-
-    def __str__(self):
-        return str(self.year) + '-' + str(self.rating)
-
-
-class WeatherHistory(models.Model):
-    date = models.DateField()
-    time = models.IntegerField()
-    icon = models.CharField(max_length=200)
-    summary = models.CharField(max_length=200)
-    precipitationType = models.CharField(max_length=200)
-    precipitationIntensity = models.FloatField()
-    precipitationIntensityMax = models.FloatField()
-    precipitationIntensityMaxTime = models.IntegerField()
-    precipitationProbability = models.FloatField()
-    precipitationAccumulation = models.FloatField()
-    uvIndex = models.IntegerField()
-    uvIndexTime = models.IntegerField()
-    sunriseTime = models.IntegerField()
-    sunsetTime = models.IntegerField()
-    temperatureMax = models.FloatField()
-    temperatureMin = models.FloatField()
-    apparentTemperatureMax = models.FloatField()
-    apparentTemperatureMin = models.FloatField()
-    windBearing = models.IntegerField()
-    apparentTemperatureMaxTime = models.IntegerField()
-    apparentTemperatureMinTime = models.IntegerField()
-    temperatureMinTime = models.IntegerField()
-    temperatureMaxTime = models.IntegerField()
-    humidity = models.FloatField()
-    cloudCover = models.FloatField()
-    dewPoint = models.FloatField()
-    windSpeed = models.FloatField()
-    moonPhase = models.FloatField()
-    pressure = models.FloatField()
-    visibility = models.FloatField()
-    temperatureHigh = models.FloatField()
-    temperatureLow = models.FloatField()
-    temperatureHighTime = models.IntegerField()
-    temperatureLowTime = models.IntegerField()
-    apparentTemperatureHigh = models.FloatField()
-    apparentTemperatureLow = models.FloatField()
-    apparentTemperatureHighTime = models.IntegerField()
-    apparentTemperatureLowTime = models.IntegerField()
-
-    def __str__(self):
-        return self.date.strftime('%d/%m/%Y')
-
-
 class LastUpdate(models.Model):
     type = models.CharField(primary_key=True, max_length=10)
     time = models.DateTimeField()
